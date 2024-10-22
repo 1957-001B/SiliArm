fdf87e16b162f589d10ebe780f63da90711397bd
M4t D4m0n <sysyphus@radical.local>
2024-10-22 14:35:26 +0100

    Tidied Up

--- assemble.py
+++ assemble.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import List
 import unittest
 
+
 class OpCode(Enum):
     MOV = 0x00
     JMP = 0x00
@@ -11,26 +12,23 @@ class OpCode(Enum):
 class Insruction:
     mnemonic: str
     opcode: OpCode
-    args: List[str] 
-    
+    args: List[str]
 
-    def __init__(self,instruction):
+    def __init__(self, instruction):
         self.instruction = instruction
         self.parsed = self.parse()
-        self.opcode = self.get_opcode ()
+        self.opcode = self.get_opcode()
 
     def parse(self):
         self.mnemonic = self.instruction.split()[0]
 
         if len(self.instruction.split()) > 1:
-            self.args = [arg.strip() for arg in self.instruction.split()[1].split(',')]
+            self.args = [arg.strip()
+                         for arg in self.instruction.split()[1].split(',')]
 
         return self.mnemonic, self.args
 
-    
-
-
-    def get_opcode (self):
+    def get_opcode(self):
 
         match self.mnemonic:
             case ["MOV"]:
@@ -38,10 +36,8 @@ class Insruction:
             case ["JMP"]:
                 return OpCode.JMP
 
-
-    def encode (self):
+    def encode(self):
         pass
-
 
 
 # Testing
@@ -50,14 +46,15 @@ class AssemblerTests(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
         self.src = Path("./test.s")
-    
+
     def test_parse(self):
         with self.src.open() as f:
-            test_instruction1= Insruction("MOV W1,W2")
-            test_instruction2= Insruction("JMP 0x89AB")
+            test_instruction1 = Insruction("MOV W1,W2")
+            test_instruction2 = Insruction("JMP 0x89AB")
 
         self.assertEqual(test_instruction1.parsed, ('MOV', ['W1', 'W2']))
         self.assertEqual(test_instruction2.parsed, ('JMP', ['0x89AB']))
+
 
 if __name__ == '__main__':
     unittest.main()
